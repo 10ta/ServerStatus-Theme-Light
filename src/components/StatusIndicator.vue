177eaeda176a9e804f21fb6d7f9a37ddc9d91c78
10ta <84114181+10ta@users.noreply.github.com>
2024-10-04 21:54:17 +0800

    change theme to dark

--- src/components/StatusIndicator.vue
+++ src/components/StatusIndicator.vue
@@ -2,8 +2,8 @@
   <div
     class="rounded-full inline-block"
     :class="{
-      'bg-green-900': status,
-      'bg-red-900': !status,
+      'bg-green-600': status,
+      'bg-red-800': !status,
     }"
   />
 </template>
